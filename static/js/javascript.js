757b80b33baddc36bd24d9fd8278e1f50959b17c
Firenzo <firenzo_akkefietje@msn.com>
2016-08-26 16:24:12 +0200

    version 1.0
    Info about characters

---
+++ static/js/javascript.js
@@ -0,0 +1,68 @@
+$(document).ready(function() {
+
+	var app = app || { };
+
+	(function(){
+
+		app.controller = {
+			init: function(){
+				app.events.init();
+				app.actions.init();
+			}
+		},
+
+		app.vars = {
+			EU:"http://eu-bns.ncsoft.com/ingame/bs/character/search/info",
+			NA:"http://na-bns.ncsoft.com/ingame/bs/character/search/info",
+			activePage: "Europe"
+		},
+
+		app.events = {
+			init: function(){
+				this.clickNav();
+			},
+
+			clickNav: function(){
+				$( "nav ul li a.europe" ).click(function() {
+  					app.vars.activePage = "Europe";
+  					app.actions.init();
+				});
+
+				$( "nav ul li a.north-america" ).click(function() {
+  					app.vars.activePage = "North-America";
+  					app.actions.init();
+				});
+			}
+
+		},
+
+		app.actions = {
+			init: function(){
+				this.switchPage();
+				this.menuFocus();
+			},
+
+			switchPage: function(){
+				if (app.vars.activePage == "Europe"){
+					$("iframe").attr("src", app.vars.EU);
+				} else {
+					$("iframe").attr("src", app.vars.NA);
+				}
+				console.log(app.vars.activePage);
+			},
+
+			menuFocus: function(){
+				if (app.vars.activePage == "Europe"){
+					$("nav ul li a.europe").addClass("focus");
+					$("nav ul li a.north-america").removeClass("focus");
+				} else {
+					$("nav ul li a.north-america").addClass("focus");
+					$("nav ul li a.europe").removeClass("focus");
+				}
+			}
+		}
+
+		app.controller.init();
+
+	}());
+});
